f05587b3fb2e87094d741cead814a0111af60c18
Ryan Davis <ryepup@gmail.com>
2018-07-14 17:31:42 -0400

    Fetch access token from github
    * pull in `request` and `request-promise` to make the call
    * oath boilerplate

--- OauthCallback/index.js
+++ OauthCallback/index.js
@@ -1,14 +1,37 @@
+const request = require('request-promise');
+
+const defaultOptions = Object.freeze({
+    method: 'POST',
+    uri: 'https://github.com/login/oauth/access_token',
+    json: true,
+});
+
+const defaultQueryOptions = Object.freeze({
+    client_id: process.env.GithubOathClientId,
+    client_secret: process.env.GithubOathClientSecret,
+});
+
 module.exports = function (context, req) {
     const { query: { code, state } } = req;
 
-    context.log('JavaScript HTTP trigger function processed a request.');
+    const qs = { ...defaultQueryOptions, code, state };
+    const opts = { ...defaultOptions, qs };
+    context.log('calling github', opts)
 
-    // TODO: call github to get a token
+    request(opts)
+        .then(res => {
+            const { access_token } = res;
+            context.log('got response', res)
+            context.res = {
+                headers: { 'Content-Type': 'text/html' },
+                body: `<html>Thank you!
+                <script>
+                  window.opener.postMessage({type: 'GITHUB_LOGIN_COMPLETE', payload:{token:'${access_token}'}}, '${process.env.targetOrigin}');
+                  window.close();
+                </script>
+                </html>`
+            };
 
-    context.res = {
-        headers: { 'Content-Type': 'text/html' },
-        body: `<html>Hello ${code}, ${state}. TODO: render postMessage code</html>`
-    };
-
-    context.done();
+            context.done();
+        });
 };
