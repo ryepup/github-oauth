6fcaf418d9e24d12d857d4484ef075f6d81d590d
Ryan Davis <ryepup@gmail.com>
2018-07-14 17:58:17 -0400

    Add some plumbing
    * reduce the noise we publish
    * basic editorconfig

--- OauthCallback/index.js
+++ OauthCallback/index.js
@@ -1,37 +1,37 @@
 const request = require('request-promise');
 
 const defaultOptions = Object.freeze({
-    method: 'POST',
-    uri: 'https://github.com/login/oauth/access_token',
-    json: true,
+  method: 'POST',
+  uri: 'https://github.com/login/oauth/access_token',
+  json: true,
 });
 
 const defaultQueryOptions = Object.freeze({
-    client_id: process.env.GithubOathClientId,
-    client_secret: process.env.GithubOathClientSecret,
+  client_id: process.env.GithubOathClientId,
+  client_secret: process.env.GithubOathClientSecret,
 });
 
 module.exports = function (context, req) {
-    const { query: { code, state } } = req;
+  const { query: { code, state } } = req;
 
-    const qs = { ...defaultQueryOptions, code, state };
-    const opts = { ...defaultOptions, qs };
-    context.log('calling github', opts)
+  const qs = { ...defaultQueryOptions, code, state };
+  const opts = { ...defaultOptions, qs };
+  context.log('calling github', opts)
 
-    request(opts)
-        .then(res => {
-            const { access_token } = res;
-            context.log('got response', res)
-            context.res = {
-                headers: { 'Content-Type': 'text/html' },
-                body: `<html>Thank you!
-                <script>
-                  window.opener.postMessage({type: 'GITHUB_LOGIN_COMPLETE', payload:{token:'${access_token}'}}, '${process.env.targetOrigin}');
-                  window.close();
-                </script>
-                </html>`
-            };
+  request(opts)
+    .then(res => {
+      const { access_token } = res;
+      context.log('got response', res)
+      context.res = {
+        headers: { 'Content-Type': 'text/html' },
+        body: `<html>Thank you!
+<script>
+  window.opener.postMessage({type: 'GITHUB_LOGIN_COMPLETE', payload:{token:'${access_token}'}}, '${process.env.targetOrigin}');
+  window.close();
+</script>
+</html>`
+      };
 
-            context.done();
-        });
+      context.done();
+    });
 };
